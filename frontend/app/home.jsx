7c8e97bd2cb19469b6817fbb2efd2132e16e0e6d
Filip Liljenberg <filipliljenberg9@gmail.com>
2025-08-25 16:11:27 +0200

    added goal, not complete

--- frontend/app/home.jsx
+++ frontend/app/home.jsx
@@ -5,64 +5,67 @@ import {
   Text,
   Dimensions,
   Switch,
+  TextInput,
 } from "react-native";
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 const { width } = Dimensions.get("window");
 
 export default function Home({ user }) {
   const [toggle, setToggle] = useState(true);
+  const [loggedUser, setLoggedUser] = useState(user);
+  const { name, weeklyGoal, height, weight, username } = loggedUser;
 
-  const firstLogin = () => {
-    if (user.weeklyGoal === 0) {
+  const [tempGoal, setTempGoal] = useState("");
+  const [dailyGoal, setDailyGoal] = useState(weeklyGoal);
+
+  const loggedIn = () => {
+    const handleSave = (e) => {
+      setDailyGoal(tempGoal);
+    };
+
+    if (dailyGoal === 0) {
       return (
-        <View>
+        <>
           <View style={styles.settings}>
             <Icon name="user" size={width * 0.35} color="#264653" />
             <View>
-              <Text>Användarnamn: {user.username}</Text>
-              <Text>Namn: {user.name}</Text>
-              <Text>Längd: {user.height}cm</Text>
-              <Text>Vikt: {user.weight}kg</Text>
-              <View style={{ flexDirection: "row" }}>
-                <Text>Notiser: </Text>
-                <Switch
-                  value={toggle}
-                  onValueChange={() => setToggle(!toggle)}
-                ></Switch>
-              </View>
+              <TextInput style={styles.input} placeholder="Namn"></TextInput>
+              <TextInput style={styles.input} placeholder="Längd"></TextInput>
+              <TextInput style={styles.input} placeholder="Vikt"></TextInput>
             </View>
           </View>
-          <View>
-            <Text>{user.steps}</Text>
-            <Text>{user.weeklyGoal}</Text>
+
+          <Button title="Vidare"></Button>
+        </>
+      );
+    } else {
+      return (
+        <View>
+          <View style={styles.goals}>
+            <Text>Välj ditt dagliga steg-mål:</Text>
+            <TextInput
+              style={styles.inputGoal}
+              value={tempGoal}
+              onChangeText={setTempGoal}
+            ></TextInput>
+            <Button title="spara" onPress={handleSave}></Button>
           </View>
         </View>
       );
-    } else {
-      <View></View>;
     }
   };
 
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          flexDirection: "row",
-          height: 70,
-          backgroundColor: "#264653",
-          justifyContent: "space-between",
-          alignItems: "center",
-          paddingHorizontal: 30,
-        }}
-      >
-        <Text style={styles.text}>{user.name}</Text>
+      <View style={styles.login}>
+        <Text style={styles.text}>{name}</Text>
         <View style={{ width: 90 }}>
           <Button title="Logga ut" />
         </View>
       </View>
-      <View>{firstLogin()}</View>
+      <View>{loggedIn()}</View>
     </View>
   );
 }
@@ -70,6 +73,14 @@ export default function Home({ user }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  login: {
+    flexDirection: "row",
+    height: 70,
+    backgroundColor: "#264653",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 30,
   },
   text: {
     color: "white",
@@ -84,4 +95,32 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     gap: 30,
   },
+  goals: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 50,
+    justifyContent: "center",
+  },
+  inputGoal: {
+    borderColor: "#000000ff",
+    borderWidth: 1,
+    borderRadius: 2,
+    margin: 10,
+    padding: 10,
+    textAlign: "center",
+  },
+  input: {
+    borderBottomWidth: 1,
+    padding: 8,
+  },
 });
+
+/*
+            <View style={{ flexDirection: "row" }}>
+              <Text>Notiser: </Text>
+              <Switch
+                value={toggle}
+                onValueChange={() => setToggle(!toggle)}
+              ></Switch>
+            </View>
+*/
